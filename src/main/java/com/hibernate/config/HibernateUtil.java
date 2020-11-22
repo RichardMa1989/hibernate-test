44ee79556d351f7a582fc9738843b5637bf98f9f
majc <majc@allinpay.com>
2020-11-22 20:48:55 +0800

    uuu

--- src/main/java/com/hibernate/config/HibernateUtil.java
+++ src/main/java/com/hibernate/config/HibernateUtil.java
@@ -1,24 +1,40 @@
 package com.hibernate.config;
 import java.util.List;
-import org.hibernate.Criteria;
+import java.util.Properties;
+import org.hibernate.SQLQuery;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.dialect.MySQL5InnoDBDialect;
 import com.hibernate.entity.Books;
 
 public class HibernateUtil
 {
     public static void main(String[] args)
     {
-        Configuration configuration=new Configuration().configure();
+        Configuration configuration=new Configuration();
+//        Properties properties=new Properties();
+//        properties.put(AvailableSettings.URL,"jdbc:mysql://localhost:3306/orange?useSSL=false&serverTimezone=Asia/Shanghai");
+//        properties.put(AvailableSettings.USER,"root");
+//        properties.put(AvailableSettings.PASS,"00000000");
+//        properties.put(AvailableSettings.DIALECT,MySQL5InnoDBDialect.class.getCanonicalName());
+//        properties.put(AvailableSettings.SHOW_SQL,true);
+//        properties.put(AvailableSettings.USE_SQL_COMMENTS,true);
+//        configuration.addProperties(properties);
+        configuration.addAnnotatedClass(Books.class);
+        configuration.configure();
         try (SessionFactory sessionFactory=configuration.buildSessionFactory(); Session session=sessionFactory.openSession())
         {
-            Criteria criteria=session.createCriteria(Books.class);
-            List<Books> list=criteria.list();
-            for(Books books:list)
-            {
-                System.out.println(books.getTitle());
-            }
+            System.out.println(sessionFactory);
+            SQLQuery query=session.createSQLQuery("select null isbn,title from books");
+//            query.setResultTransformer(new AliasToBeanResultTransformer(Books.class));
+            query.addEntity(Books.class);
+            List<Books> list=query.setMaxResults(5).setFirstResult(5).list();
+            System.out.println(list.size());
+            System.out.println(list.get(0).getClass());
+            System.out.println(list.get(0).getIsbn());
+            System.out.println(list.get(0).getTitle());
         }
     }
 }
